a835f9e4c42ec49daefe3dccf3478e98dd36af2e
Afiq <afiqquraisyzulkarnain@gmail.com>
2023-05-09 10:41:17 +0800

    reconfig

--- init.lua
+++ init.lua
@@ -1,20 +1,26 @@
-do local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-        vim.fn.system({
-            "git",
-            "clone",
-            "--filter=blob:none",
-            "https://github.com/folke/lazy.nvim.git",
-            "--branch=stable", -- latest stable release
-            lazypath,
-        })
-    end
-    vim.opt.rtp:prepend(lazypath)
+if vim.o.shell:match("/fish$") then
+	vim.o.shell = "bash"
 end
 
-vim.g.mapleader = ' ' -- make sure mapping is correct
+do
+	local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+	if not vim.loop.fs_stat(lazypath) then
+		vim.fn.system({
+			"git",
+			"clone",
+			"--filter=blob:none",
+			"https://github.com/folke/lazy.nvim.git",
+			"--branch=stable", -- latest stable release
+			lazypath,
+		})
+	end
+	vim.opt.rtp:prepend(lazypath)
+end
 
-local cmd = io.popen "ls $NVIMLUA"
+vim.g.mapleader = " " -- make sure mapping is correct
+
+local cmd = io.popen("ls $NVIMLUA")
 for f in cmd:lines() do
-    require(f:gsub("%.lua$",""))
-end; cmd:close()
+	require(f:gsub("%.lua$", ""))
+end
+cmd:close()
--- lua/vim-conf.lua
+++ lua/vim-conf.lua
@@ -1,39 +1,52 @@
-do
-	local function set_vopt(...)
-		for i = 1,select('#',...),2 do
-			local opt_name, opt_value = select(i,...), select(i+1,...)
-
-			vim.opt[opt_name] = opt_value
-		end
+local function set_opt(tbl)
+	for i, v in pairs(tbl) do
+		vim.opt[i] = v
 	end
-
-	local vopt = vim.opt
-
-	set_vopt(
-	-- Set number
-	  "number", true
-
-    -- Set mouse
-	, "mouse", vopt.mouse._value..'a'
-
-	-- Set indent to 4 spaces
-	, "autoindent", true
-	, "expandtab", true
-	, "tabstop", 4
-	, "shiftwidth", 4
-	)
 end
+
+set_opt({
+	-- Set Number
+	number = true,
+	-- Set indent to 4 spaces
+	autoindent = true,
+	expandtab = true,
+	tabstop = 4,
+	shiftwidth = 4,
+})
 
 -- patch WSL clipboard
 vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-        ["+"] = "clip.exe",
-        ["*"] = "clip.exe"
-    },
-    paste = {
-        ["+"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
-        ["*"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]]
-    },
-    cache_enabled = 0
+	name = "WslClipboard",
+	copy = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe",
+	},
+	paste = {
+		["+"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+		["*"] = [[powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))]],
+	},
+	cache_enabled = 0,
 }
+
+local function stylua_cmd(save)
+	return function(opts)
+		local filename = opts.fargs[1] or nil
+
+		if save and vim.fn.getbufinfo("%")[1].changed == 1 then
+			vim.cmd"w"
+		end
+
+		if not filename then
+			vim.cmd("term stylua % --color always --check %\
+            startinsert")
+		else
+			vim.cmd("term stylua "..table.concat(opts.fargs," ").."\
+            startinsert")
+		end
+	end
+end
+
+vim.api.nvim_create_user_command("SStylua", stylua_cmd(true), { nargs = "*" })
+vim.api.nvim_create_user_command("Stylua", stylua_cmd(), { nargs = "*" })
+
+
